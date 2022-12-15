0a905ca903e887ae631b45cb84d61c04a9e5b04b
Egor251 <egorpyshny@gmail.com>
2022-12-15 14:45:08 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import {StartPage} from './components/startpage'
+import {TimeLine} from './components/bio/valmen/timeline'
+import { Valmen } from './components/bio/valmen/valmen';
+import { Head } from './components/header';
+import { List } from './components/list';
+import {Belogortsev} from './components/bio/belogortsev/belogortsev';
+import { Korol } from './components/bio/korol/korol';
+import { Zaborski } from './components/bio/zaborski/zaborski';
+import { Baklanov } from './components/bio/baklanov/baklanov';
 
 function App() {
+
+const{t, i18n} = useTranslation();
+
+const changeLanguage = (lang) =>{
+  i18n.changeLanguage(lang);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Head/>
+      <Switch>
+        <Route exact path ="/" component = {StartPage}/>
+        <Route path="/list" component = {List}/>  
+        <Route path="/Valmen" component ={Valmen}/>
+        <Route path="/Belogortsev" component ={Belogortsev}/>
+        <Route path="/Korol" component ={Korol}/>
+        <Route path="/Zaborski" component ={Zaborski}/>
+        <Route path="/Baklanov" component ={Baklanov}/>
+      </Switch>
+    </Router>
+    
   );
 }
 
